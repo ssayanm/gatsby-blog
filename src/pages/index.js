67f730a5f88b948c11d6b2ac44d80c536ce8dbf9
Sayan Mukherjee <hello@sayanmukherjee.com>
2020-02-11 09:29:34 +0530

    added styled component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,19 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+
+const BlogLink = styled(Link)`
+  text-decoration: none;
+`
+
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+  color: green;
+`
 
 export default ({ data }) => (
   <Layout>
@@ -13,9 +23,11 @@ export default ({ data }) => (
       <h4>{data.allMarkdownRemark.totalCount}</h4>
       {data.allMarkdownRemark.edges.map(({ node }) => (
         <div key={node.id}>
-          <span>
-            {node.frontmatter.title} - {node.frontmatter.date}
-          </span>
+          <BlogLink to={node.fields.slug}>
+            <BlogTitle>
+              {node.frontmatter.title} - {node.frontmatter.date}
+            </BlogTitle>
+          </BlogLink>
           <p>{node.excerpt}</p>
         </div>
       ))}
@@ -25,7 +37,7 @@ export default ({ data }) => (
 
 export const query = graphql`
   query {
-    allMarkdownRemark {
+    allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
       edges {
         node {
           id
@@ -33,6 +45,9 @@ export const query = graphql`
             title
             description
             date
+          }
+          fields {
+            slug
           }
           excerpt
         }
