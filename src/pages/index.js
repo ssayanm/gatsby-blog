4ccb4f2d76d874fcdfeaa10fb1075214b210ed08
Sayan Mukherjee <hello@sayanmukherjee.com>
2020-02-12 10:05:28 +0530

    added image src

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { graphql, Link } from "gatsby"
 import styled from "styled-components"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+// import Image from "../components/image"
 import SEO from "../components/seo"
 
 const BlogLink = styled(Link)`
@@ -14,12 +14,16 @@ const BlogTitle = styled.h3`
   margin-bottom: 20px;
   color: green;
 `
+const Image = styled.img`
+  width: 150px;
+`
 
 export default ({ data }) => (
   <Layout>
     <SEO title="Home" />
     <div>
       <h1>Sayan's Thoughts</h1>
+      <Image src={require("./../images/gatsby-astronaut.png")} />
       <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
       {data.allMarkdownRemark.edges.map(({ node }) => (
         <div key={node.id}>
